8790f793836993ae295f438c7e289736f5cb2652
aspandyar <aspandyart@gmail.com>
2024-08-17 13:42:58 +0500

    fixing start script

--- start.sh
+++ start.sh
@@ -4,9 +4,13 @@ set -e
 
 echo "run db migration"
 
+cat /app/app.env
+
 . /app/app.env
 
 echo "DB_SOURCE is set to: $DB_SOURCE"
+
+source /app/app.env
 
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
