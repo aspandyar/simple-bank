5eb1ca217e4d0e1a1aca315ad206fdfa076af32b
aspandyar <aspandyart@gmail.com>
2024-08-17 13:39:45 +0500

    working with source db

--- start.sh
+++ start.sh
@@ -3,7 +3,11 @@
 set -e
 
 echo "run db migration"
+
 . /app/app.env
+
+echo "DB_SOURCE is set to: $DB_SOURCE"
+
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
