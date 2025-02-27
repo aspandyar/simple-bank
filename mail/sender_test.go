d7d62a5a5d696756320bb151dd9e064470d81260
aspandyar <aspandyart@gmail.com>
2025-02-27 10:38:41 +0500

    setting email sender (test error is okay)

--- mail/sender_test.go
+++ mail/sender_test.go
@@ -1,7 +1,28 @@
 package mail
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/aspandyar/simple-bank/util"
+	"github.com/stretchr/testify/require"
+)
 
 func TestSendEmailWithGmail(t *testing.T) {
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
 
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+		<h1>Hello World!</h1>
+		<p>This is a test email from the <b>Simple Bank</b> application.</p>
+		
+		<p>Sorry for the spam. Please ignore this email.</p>
+	`
+	to := []string{"stephen.novel@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
 }
